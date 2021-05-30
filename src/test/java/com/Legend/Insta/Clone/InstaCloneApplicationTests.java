590887c44a34b849a4d4ab9bba28c3f19ab31ad0
unknown <simondangol.26@gmail.com>
2021-05-30 09:14:59 +0545

    Inital Project Setup

---
+++ src/test/java/com/Legend/Insta/Clone/InstaCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Legend.Insta.Clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
